92ef3a2ef41eb861bca032645f9bf9671e90960d
Manuel Diaz <yosoymacnolo@gmail.com>
2020-07-17 16:22:31 -0500

    Reverting License and Readme

--- debugger/build.gradle.kts
+++ debugger/build.gradle.kts
@@ -1,22 +1,6 @@
 plugins {
     kotlin("jvm")
     application
-<<<<<<< 190e36fabcb657062ec442b74e6d96a2972e0590
-    id("org.openjfx.javafxplugin") version "0.0.8"
-}
-
-group = "org.wint3794"
-version = "0.6.1"
-
-application {
-    applicationName = "Graphical Debugger"
-    mainClassName = "org.wint3794.pathfollower.debugger.Main"
-}
-
-javafx {
-    modules("javafx.controls", "javafx.fxml")
-}
-=======
 }
 
 application {
@@ -26,7 +10,6 @@ application {
 
 group = "org.wint3794"
 version = "1.0.0"
->>>>>>> Adapting JVM 1.8 for Android Support
 
 repositories {
     mavenCentral()
